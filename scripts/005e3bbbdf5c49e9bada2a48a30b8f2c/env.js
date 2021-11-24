412476d4a550fe3637cac437dfb2ed8e25e560a6
晓旭 <1767332988@qq.com>
2021-11-25 03:40:22 +0800

    qqbot

---
+++ scripts/005e3bbbdf5c49e9bada2a48a30b8f2c/env.js
@@ -0,0 +1,6 @@
+process.env.JD_COOKIE="pt_key=AAJhkQiDADCfvZPPRRqIFoL2Odg-53YiAeQCuoT1FL12yRmJjIX74sb6-sZowS6G3dqMZv5Kkrk; pt_pin=jd_4ef4d77013f57;&pt_key=AAJhki5LADCGjzP2YTMTrzANlg9fOh1PVtM7Yz7BW0mKd4y2VwIXdDu1PttJ_fOlj4FU3Qd-1go; pt_pin=jd_41971542df482;"
+process.env.GOBOT_URL="http://172.17.0.1:8000/send_private_msg?user_id=2164798590"
+process.env.Push="true"
+
+process.env.ServicePort="5010"
+process.env.qq="2164798590"
